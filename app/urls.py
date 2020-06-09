d6624a895c18a846d4764bc1a92a198f32b8dbe2
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-10 00:16:14 +0900

    書籍詳細追加

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-  path('', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('detail/<str:isbn>', views.DetailView.as_view(), name='detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -7,47 +7,90 @@ from .forms import SearchForm
 SEARCH_URL = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&applicationId=1025176530386995193'
 
 def get_api_data(params):
-  api = requests.get(SEARCH_URL, params=params).text
-  result = json.loads(api)
-  items = result['Items']
-  return items
+    api = requests.get(SEARCH_URL, params=params).text
+    result = json.loads(api)
+    items = result['Items']
+    return items
 
 class IndexView(View):
-  def get(self, request, *args, **kwargs):
-    form = SearchForm(request.POST or None)
+    def get(self, request, *args, **kwargs):
+        form = SearchForm(request.POST or None)
 
-    return render(request, 'app/index.html', {
-      'form': form
-    })
+        return render(request, 'app/index.html', {
+            'form': form
+        })
 
-  def post(self, request, *args, **kwargs):
-    form = SearchForm(request.POST or None)
+    def post(self, request, *args, **kwargs):
+        form = SearchForm(request.POST or None)
 
-    if form.is_valid():
-      keyword = form.cleaned_data['title']
-      params = {
-        'title': keyword,
-        'hits': 28,
-      }
-      items = get_api_data(params)
-      book_data = []
-      for i in items:
-        item = i['Item']
+        if form.is_valid():
+            keyword = form.cleaned_data['title']
+            params = {
+                'title': keyword,
+                'hits': 28,
+            }
+            items = get_api_data(params)
+            book_data = []
+            for i in items:
+                item = i['Item']
+                title = item['title']
+                image = item['largeImageUrl']
+                isbn = item['isbn']
+                query = {
+                'title': title,
+                'image': image,
+                'isbn': isbn
+                }
+                book_data.append(query)
+
+            return render(request, 'app/book.html', {
+                'book_data': book_data,
+                'keyword': keyword
+            })
+    
+        return render(request, 'app/index.html', {
+            'form': 'form'
+        })
+
+class DetailView(View):
+    def get(self, request, *args, **kwargs):
+        isbn = self.kwargs['isbn']
+        params = {
+            'isbn': isbn
+        }
+
+        items = get_api_data(params)
+        items = items[0]
+        item = items['Item']
         title = item['title']
         image = item['largeImageUrl']
+        author = item['author']
+        itemPrice = item['itemPrice']
+        salesDate = item['salesDate']
+        publisherName = item['publisherName']
+        size = item['size']
         isbn = item['isbn']
-        query = {
-          'title': title,
-          'image': image,
-          'isbn': isbn
-        }
-        book_data.append(query)
+        itemCaption = item['itemCaption']
+        itemUrl = item['itemUrl']
+        reviewAverage = item['reviewAverage']
+        reviewCount = item['reviewCount']
 
-      return render(request, 'app/book.html', {
-        'book_data': book_data,
-        'keyword': keyword
-      })
-    
-    return render(request, 'app/index.html', {
-      'form': 'form'
-    })
+        book_data = {
+            'title': title,
+            'image': image,
+            'author': author,
+            'itemPrice': itemPrice,
+            'salesDate': salesDate,
+            'publisherName': publisherName,
+            'size': size,
+            'isbn': isbn,
+            'itemCaption': itemCaption,
+            'itemUrl': itemUrl,
+            'reviewAverage': reviewAverage,
+            'reviewCount': reviewCount,
+            'average': float(reviewAverage) * 20,
+        }
+
+        return render(request, 'app/detail.html', {
+            'book_data': book_data
+        })
